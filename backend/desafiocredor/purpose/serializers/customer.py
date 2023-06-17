f99b26f330446d0e252efd1040fcee78e081d378
Alice Lima <leriasler@gmail.com>
2023-06-17 14:17:23 -0300

    serializers

---
+++ backend/desafiocredor/purpose/serializers/customer.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from ..models import Customer
+
+class CustomerSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=150)
+    cpf = serializers.IntegerField()
+    adress = serializers.CharField(max_length=200)
+
+    def validate_name(self, name: str) -> str:
+        if not name:
+            raise serializers.ValidationError("Insert a valid name")
+        return name
+    
+    def validate_cpf(self, cpf: int) -> int:
+        if not cpf:
+            raise serializers.ValidationError("Insert a valid cpf")
+        if len(cpf) != 11:
+            raise serializers.ValidationError("Cpf must be 11 digits")
+        return cpf
+    
+    def validate_adress(self, adress: str) -> str:
+        if not adress:
+            raise serializers.ValidationError("Insert a valid adress")
+        return adress
+
+    def create(self, validated_data) -> Customer:
+        return Customer.objects.create(**validated_data)
+
+    class Meta:
+        model = Customer
+        fields = ['id', 'name', 'cpf', 'adress']
---
+++ backend/desafiocredor/purpose/serializers/purpose.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from ..models import Purpose
+from .customer import CustomerSerializer
+
+class PurposeSerializer(serializers.ModelSerializer):
+    customer = CustomerSerializer(read_only=True)
+    value = serializers.IntegerField()
+
+    def validate_customer(self, customer: str) -> str:
+        if not customer:
+            raise serializers.ValidationError("Customer not be null")
+
+    def validate_value(self, value: int) -> int:
+        if value <= 0:
+            raise serializers.ValidationError("Value must be greater than zero.")
+        return value
+    
+    def create(self, validated_data) -> Purpose:
+        return Purpose.objects.create(**validated_data)
+    
+    class Meta:
+        model = Purpose
+        fields = ['id', 'customer', 'value', 'accepted']
+        
