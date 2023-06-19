aa1ff39c3b2381cf9aa92f84df4d662dd651154d
Alice Lima <leriasler@gmail.com>
2023-06-19 14:40:10 -0300

    create task

--- backend/desafiocredor/api/tasks.py
+++ backend/desafiocredor/api/tasks.py
@@ -1,8 +1,20 @@
+import random
+
 from celery import shared_task
 
-from api.models import Proposal
+from .models import Proposal
 
 
 @shared_task
-def process_proposal():
-    pass
+def process_proposal(proposal_id):
+    print("Message received!")
+    try:
+        proposal_obj = Proposal.objects.get(id=proposal_id)
+        approved = bool(random.randint(0, 1))
+        proposal_obj.status = "approved" if approved else "denied"
+        proposal_obj.save()
+        print(f"Proposal {proposal_obj.status}")
+    except Proposal.DoesNotExist:
+        print("Proposal not found")
+
+
